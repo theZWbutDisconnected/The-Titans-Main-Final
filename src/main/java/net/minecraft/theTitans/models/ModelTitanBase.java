1e380bc2517ef65530aabbb22b05dbd7c37d0c84
theZWbutDisconnected <2090252203@qq.com>
2025-01-04 20:36:54 +0800

    Animator

---
+++ src/main/java/net/minecraft/theTitans/models/ModelTitanBase.java
@@ -0,0 +1,50 @@
+package net.minecraft.theTitans.models;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import it.unimi.dsi.fastutil.objects.ObjectList;
+import net.minecraft.client.renderer.entity.model.EntityModel;
+import net.minecraft.client.renderer.model.ModelRenderer;
+import net.minecraft.entity.titan.EntityTitan;
+import java.lang.reflect.Field;
+
+public abstract class ModelTitanBase<T extends EntityTitan> extends EntityModel<T>
+{
+	public final ObjectList<ModelRenderer> childBones = new ObjectArrayList<>();
+
+	@Override
+    public void prepareMobModel(T p_212843_1_, float p_212843_2_, float p_212843_3_, float p_212843_4_) {
+        for (ModelRenderer i : this.getChilds()) {
+			childBones.add(i);
+			Class c = i.getClass();
+			Field f = null;
+			try {
+				f = c.getDeclaredField("children");
+			} catch (NoSuchFieldException e) {
+				try {
+					f = c.getDeclaredField("field_78805_m");
+				} catch (NoSuchFieldException e1) {}
+			}
+			try {
+				for (ModelRenderer j : (ObjectList<ModelRenderer>)(f.get(i))) {
+					if (!this.has(this.getChilds(), j))
+						childBones.add(j);
+				}
+			} catch (IllegalAccessException e) {} catch (IllegalArgumentException e) {}
+		}
+        super.prepareMobModel(p_212843_1_, p_212843_2_, p_212843_3_, p_212843_4_);
+    }
+	
+	private boolean has(ModelRenderer[] ms, ModelRenderer m) {
+		for (ModelRenderer i : ms) {
+			if (i == m) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public ModelRenderer[] getChilds() {
+		return new ModelRenderer[] {};
+	}
+}
