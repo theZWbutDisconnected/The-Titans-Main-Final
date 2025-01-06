dab817251e906ad4247ef2bf250788679f68af6d
theZWbutDisconnected <2090252203@qq.com>
2025-01-06 21:34:36 +0800

    Try to fix IronGolemTitan Collision push

--- src/main/java/net/minecraft/entity/titan/EntityIronGolemTitan.java
+++ src/main/java/net/minecraft/entity/titan/EntityIronGolemTitan.java
@@ -1,6 +1,6 @@
 package net.minecraft.entity.titan;
 
-import java.annotation.Nullable;
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
